fb14f45973c362f5cacc15b67ec8f4c428a78c8a
Reine0017 <m.fortitudo.fr@gmail.com>
2020-06-19 21:46:17 +0700

    homePage

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -2,12 +2,16 @@ import React from "react";
 
 import HomeSlider from "./slider";
 import Subscriptions from "../utils/subscribe";
+import HomeArticles from "./articles";
 
 function Home() {
   return (
     <>
       <HomeSlider />
       <Subscriptions />
+      <div className="container">
+        <HomeArticles />
+      </div>
     </>
   );
 }
