184fad6f011538f7ed3ae4b87f37198757b71332
Reine0017 <m.fortitudo.fr@gmail.com>
2020-06-20 00:50:39 +0700

    use cookies and cast no more than three votes

--- src/components/utils/poll.js
+++ src/components/utils/poll.js
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import cookie from "react-cookies";
 
 import { URL_TEAMS } from "./paths";
 
 function Poll() {
   const [pollTeams, setPollTeams] = useState([]);
   const [error, setError] = useState(false);
+  const [clickCount, setClickCount] = useState(1);
 
   const getPoll = () => {
     axios
@@ -15,10 +17,36 @@ function Poll() {
       });
   };
 
+  const addCount = (count, id) => {
+    let getCookie = cookie.load("poll");
+    console.log(getCookie);
+    console.log(clickCount);
+
+    if (getCookie === undefined || getCookie < 3) {
+      axios(`${URL_TEAMS}/${id}`, {
+        method: "PATCH",
+        headers: {
+          "Content-type": "application/json",
+        },
+        data: JSON.stringify({ count: count + 1 }),
+      }).then((response) => {
+        cookie.save("poll", clickCount);
+        setClickCount(clickCount + 1);
+        getPoll();
+      });
+    } else {
+      setError(true);
+    }
+  };
+
   const showPoll = () => {
     const position = ["1ST", "2ND", "3RD"];
     return pollTeams.map((item, index) => (
-      <div key={index} className="poll_item">
+      <div
+        key={index}
+        className="poll_item"
+        onClick={() => addCount(item.count, item.id)}
+      >
         <>{console.log(item)}</>
         <>{console.log(item.logo)}</>
         <img alt={item.team} src={`/images/teams/${item.logo}`} />
@@ -30,6 +58,7 @@ function Poll() {
 
   useEffect(() => {
     getPoll();
+    //cookie.remove("poll");
   }, []);
   console.log(pollTeams);
   return (
@@ -38,6 +67,9 @@ function Poll() {
         <h3>Who will be the next champion?</h3>
         <>
           <div className="poll_container">{showPoll()}</div>
+          {error ? (
+            <div>Sorry, you cannot cast more than three votes</div>
+          ) : null}
         </>
       </div>
     </>
