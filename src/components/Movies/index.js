c194b3cc8da37db7519eca25c3e51cb9f6f920bb
Reine0017 <m.fortitudo.fr@gmail.com>
2020-06-22 23:50:27 +0700

    fetch onClick

--- src/components/Movies/index.js
+++ src/components/Movies/index.js
@@ -3,13 +3,13 @@ import { useSelector, useDispatch } from "react-redux";
 import { getMoviesList } from "../../actions";
 
 function Movies() {
+  //this will become: const movies = state.movies
+  const movies = useSelector((state) => state.movies);
   const dispatch = useDispatch();
 
   // useEffect(() => {
   //   dispatch(getMoviesList());
   // }, [dispatch]);
-
-  const movies = useSelector((state) => state.movies);
 
   console.log(movies);
   return (
