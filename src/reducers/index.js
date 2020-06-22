2fc863b0a2ad156fe26dd3c7d4aac3b2483ff603
Reine0017 <m.fortitudo.fr@gmail.com>
2020-06-22 21:36:24 +0700

    added comments

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,6 +2,7 @@ import { combineReducers } from "redux";
 import movies from "./movies_reducers";
 
 const rootReducer = combineReducers({
+  //this means movies:movies this is what the payload is called
   movies,
 });
 
